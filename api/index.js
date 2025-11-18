13653ffcf161a992455b48ba36cd091a198d2c1a
ypatodkar <ypatodkar@scu.edu>
2025-11-18 13:08:10 -0800

    Final commit

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function wrapper for Express app
-import app from '../backend/server.js';
-
-// Vercel expects a handler function
-// Export the Express app - Vercel's @vercel/node will handle it
-export default app;
-
